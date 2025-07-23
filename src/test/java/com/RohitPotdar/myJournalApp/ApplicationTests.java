d3658b635ee713a16974d58ddbe70095c246809a
Rohitpotdar44 <potdarrohit59@gmail.com>
2025-07-23 23:48:17 +0530

    Add test cases

---
+++ src/test/java/com/RohitPotdar/myJournalApp/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RohitPotdar.myJournalApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
